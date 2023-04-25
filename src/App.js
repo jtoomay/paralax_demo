46f7cf037c860cee118487cacc9ad74789efa0bf
John Doe <jake@tooamay.dev>
2023-04-24 22:16:07 -0500

    Simple Spring Transition

--- src/App.js
+++ src/App.js
@@ -3,22 +3,43 @@ import './App.css';
 import tw, { styled } from 'twin.macro';
 import { useState } from 'react';
 
-// React Spring
-import { Parallax, ParallaxLayer } from '@react-spring/web'
+//* React Spring
+// import { Parallax, ParallaxLayer } from '@react-spring/web'
+import { useSpring, animated } from '@react-spring/web'
 
 function App() {
 
+  //* One value is an array of springs, the other is a function to update them
+  const [springs, api] = useSpring(() => ({
+    from: { x: 0}, 
+  }))
 
+  //* Function that will translate the div
+  const handleClick = () => { 
+    api.start({ 
+      from: {
+        x: 0,
+      }, 
+      to: {
+        x: 100,
+      },
+
+    })
+  }
 
   return (
     <>
-      <Parallax pages={2} style={{top: '0', left: '0'}}>
-        <ParallaxLayer
-          offset={0}
-          speed={0.5}
-          style={{ backgroundColor: '#805E73' }}
-        />
-      </Parallax>
+  
+      <animated.div
+      onClick={handleClick}
+        style={{
+          width: 80, 
+          height: 80,
+          backgroundColor: 'red',
+          borderRadius: 16,
+          ...springs
+        }}
+      />
     </>
   );
 }
