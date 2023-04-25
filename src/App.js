0f76f508edadb514d7796c06963754fb9522cacc
John Doe <jake@tooamay.dev>
2023-04-24 20:54:17 -0500

    Initial Project setup/cleanup. Installed React Spring

--- src/App.js
+++ src/App.js
@@ -9,28 +9,12 @@ function App() {
 
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-       
-          <Test toggle={toggle}>Test Component</Test>
-          <TestButton onClick={() => setToggle(!toggle)}>Click me to test styledComponents</TestButton>
-          Learn React
-      
-      </header>
-    </div>
+    <>
+      <h1>We all setup now... Nobody can stop this</h1>
+      <h1>Hello</h1>
+    </>
   );
 }
 
 export default App;
 
-const TestButton = tw.button`text-white p-4 bg-blue-500 rounded-xl my-10`
-
-
-const Test = styled.p(({toggle}) => [
-  tw`text-red-500`,
-  toggle && tw`text-blue-500`,
-])
